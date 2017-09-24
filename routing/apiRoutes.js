426de5c750b28fe5f9e7f98b23ac9765cb028a47
Sofia Wicker <sofiazw@gmail.com>
2017-09-24 16:57:55 -0400

    update

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -4,7 +4,7 @@ var fetch = require("node-fetch")
 var router = express.Router();
 var Key = process.env.Key
 
-var apiURL = "https://api.wunderground.com/api/" + Key + "/geolookup/conditions/q/"
+var apiURL = "https://api.wunderground.com/api/92f22e9a41ea4efb/geolookup/conditions/q/"
 
 //GET route for getting all of the white cards
 router.get("/:state/:city", function (req, res) {
